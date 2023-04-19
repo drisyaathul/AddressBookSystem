3a9813f9b4336f461a3d7c79ba1d948b66444e15
drisyaathul <drisyak809@gmail.com>
2023-04-20 00:22:21 +0800

    Added: Edit the Existing Contact in Address Book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -11,7 +11,7 @@ public class AddressBook {
     ArrayList<ContactPerson> contact = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
 
-    public void createContact() {
+    public void addContact() {
 /*
     Creating Contacts for each person by Using Scanner method
  */
@@ -34,12 +34,80 @@ public class AddressBook {
         contactPerson.setEmail(scanner.nextLine());
         contact.add(contactPerson);
     }
-    public void addContact(){
-/*
-    To Print the ArrayList of Contact
- */
-        System.out.println(contact);
-        System.out.println("Contact Added Successfully");
+    public void editContact(String searchFirstName) {
+ /*
+    * To Edit Existing Contact Person by Searching their First Name
+    * By using if Loop, list of the existing contact will print
+    * By using Switch Case, Updates the existing contact Person
 
+ */
+        for (ContactPerson contactPerson : contact) {
+            String firstName = contactPerson.getFirstName();
+            if (firstName.equals(searchFirstName)) {
+                System.out.println("Contact Details of "+searchFirstName+" :-");
+                System.out.println("1. First Name \n2. Last Name \n3. Address \n4. City \n5. State \n6. Zip Code \n7. Phone Number \n8. Email Id \n");
+                System.out.println("Enter your Choice which you want to Edit :- ");
+                int choice = scanner.nextInt();
+
+                switch (choice){
+                    case 1:
+                        System.out.println("Update the First Name :");
+                        String updatedFirstName = scanner.next();
+                        contactPerson.setFirstName(updatedFirstName);
+                        System.out.println("First Name is UPDATED");
+                        break;
+                    case 2:
+                        System.out.println("Update the Last Name :");
+                        String updatedLastName = scanner.next();
+                        contactPerson.setLastName(updatedLastName);
+                        System.out.println("Last Name is UPDATED");
+                        break;
+                    case 3:
+                        System.out.println("Update the Address :");
+                        String updatedAddress = scanner.next();
+                        contactPerson.setAddress(updatedAddress);
+                        System.out.println("Address is UPDATED");
+                        break;
+                    case 4:
+                        System.out.println("Update the City :");
+                        String updatedCity = scanner.next();
+                        contactPerson.setCity(updatedCity);
+                        System.out.println("City is UPDATED");
+                        break;
+                    case 5:
+                        System.out.println("Update the State :");
+                        String updatedState = scanner.next();
+                        contactPerson.setState(updatedState);
+                        System.out.println("State is UPDATED");
+                        break;
+                    case 6:
+                        System.out.println("Update the Zip :");
+                        String updatedZip = scanner.next();
+                        contactPerson.setZip(updatedZip);
+                        System.out.println("Zip Code is UPDATED");
+                        break;
+                    case 7:
+                        System.out.println("Update the Phone Number :");
+                        String updatedPhoneNumber = scanner.next();
+                        contactPerson.setPhoneNumber(updatedPhoneNumber);
+                        System.out.println("Phone Number is UPDATED");
+                        break;
+                    case 8:
+                        System.out.println("Update the Email Id :");
+                        String updatedEmailId = scanner.next();
+                        contactPerson.setEmail(updatedEmailId);
+                        System.out.println("Email Id is UPDATED");
+                        break;
+                    default:
+                        System.out.println("INVALID CHOICE");
+                    }
+                }
+            else
+                System.out.println("NO RECORD FOUND !");
+            }
+        }
+        public void display(){
+            // To print the ArrayList of Contact
+            System.out.println(contact);
+        }
     }
-}
